7e0cbf2c876d0497cf0679769b849c5544e65db2
Jarkko Kärki <152171266+JarkkoKarki@users.noreply.github.com>
2025-09-10 19:45:11 +0300

    Chapter3

---
+++ Chapter3/Task1/main.py
@@ -0,0 +1,60 @@
+
+
+class ListNode():
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+    
+    def __repr__(self):
+        return f'<ListNode: {self.data}>'
+
+class SinglyLinkedList():
+    def __init__(self):
+        self._head = None
+
+    def __repr__(self):
+        current_node = self._head
+        values = ''
+        while current_node:
+            values += f', {current_node.data}'
+            current_node = current_node.next
+        return f'<SinglyLinkedList: [{values.lstrip(", ")}]>'
+     
+    def append(self, value):
+        """
+        Append a value to the end of the list
+
+        Parameters:
+        - 'value': The data to append
+
+        Returns: None
+        """
+        # Create the node with the value
+        node = ListNode(value)
+        # If list is empty just point the header to the new node
+        if not self._head:
+            self._head = node
+        else:
+            # if list is not empty, search for the last element and point it to the new node
+            current_node = self._head
+            while current_node.next != None:
+                current_node = current_node.next
+            current_node.next = node
+    def pop(self):
+        if not self._head:
+            return None
+
+        if not self._head.next:
+            value = self._head.data
+            self._head = None
+            return value
+
+        current_node = self._head
+        while current_node.next.next:
+            current_node = current_node.next
+
+        value = current_node.next.data
+        current_node.next = None
+        return value
+                
+        
---
+++ Chapter3/Task2/main.py
@@ -0,0 +1,71 @@
+class ListNode():
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+    
+    def __repr__(self):
+        return f'<ListNode: {self.data}>'
+
+class SinglyLinkedList():
+    def __init__(self):
+        self._head = self._tail = None
+        self._size = 0
+
+    def __repr__(self):
+        current_node = self._head
+        values = ''
+        while current_node:
+            values += f', {current_node.data}'
+            current_node = current_node.next
+        plural = '' if self._size == 1 else 's'
+        return f'<SinglyLinkedList ({self._size} element{plural}): [{values.lstrip(", ")}]>'
+
+    def __len__(self):
+        return self._size
+
+    def append(self, value):
+        """
+        Append a value to the end of the list
+
+        Parameters:
+        - 'value': The data to append
+
+        Returns: None
+        """
+        # Create the node with the value
+        new_node = ListNode(value)
+
+        # If list is empty just point the header to the new node
+        if not self._tail:
+            self._head = new_node
+            self._tail = new_node
+        else:
+            # if list is not empty, update the last element and point it to the new node
+            self._tail.next = new_node
+            self._tail = new_node
+
+        # Update list's size
+        self._size += 1
+
+    def pop(self):
+        if not self._head:
+            return None
+
+        if not self._head.next:
+            value = self._head.data
+            self._head = None
+            self._tail = None
+            self._size -=1
+            return value
+
+        current_node = self._head
+        while current_node.next.next:
+            current_node = current_node.next
+
+        value = current_node.next.data
+        current_node.next = None
+        self._tail = current_node
+        self._size -= 1
+        return value
+                
+        
